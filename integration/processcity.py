ca8fdfaef910da95d1a95c2cace2cf5d2e13d2ba
ay-yang <34326968+ay-yang@users.noreply.github.com>
2018-06-12 14:36:26 -0400

    test with google cloud

--- integration/processcity.py
+++ integration/processcity.py
@@ -1,14 +1,49 @@
-#processcity.py
+#!C:\Python\python.exe
+import numpy
 import os
 import cgi
 
 #os.environ['city'] = 'beijing'
-city = os.environ.get('city')
+#city = os.getenv('city')
+form=cgi.FieldStorage() 
+city = form.getvalue("city")
 
-print (city)
-
+def load():
+	if(city=="montreal"):
+		trained_model=model.LSTM(12,24,2)
+		states=torch.load('montreal.pt')
+		trained_model.load_state_dict(states)
+		
+		return trained_model
+		
+	if (city =="toronto"):
+		trained_model=model.LSTM(13,24,2)
+		states=torch.load('toronto.pt')
+		trained_model.load_state_dict(states)
+		
+		return trained_model
+		
+	if (city =="ottawa"):
+		trained_model=model.LSTM(13,24,2)
+		states=torch.load('ottawa.pt')
+		trained_model.load_state_dict(states)
+		
+		return trained_model
+		
+	if (city =="vancouver"):
+		trained_model=model.LSTM(16,24,2)
+		states=torch.load('ottawa.pt')
+		trained_model.load_state_dict(states)
+		
+		return trained_model
+		
+#def compute(model):
+	#print("ok")
+	
 def main():
-	print ("Content-Type: text/html \r\n\r\n")
+	print ("Content-Type: text/html")
+	print("\r\n")
+	print()
 	print ("""
 	<!DOCTYPE html>
 	<html>
@@ -17,7 +52,5 @@ def main():
 	print("""
 	</body>
 	</html>""")
-	
-	if(city == "beijing")
-	
+
 main()
